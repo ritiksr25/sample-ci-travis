1c44b0b0a93e299265f8f30e9279732c1a4482c2
ritik srivastava <ritiksrivastava025@gmail.com>
2019-06-14 16:50:46 +0530

    branch

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.get('/api/v1/', (req, res) => {
         { name: 'User1', email: 'user1@gmail.com' },
         { name: 'User2', email: 'user2@gmail.com' },
         { name: 'User3', email: 'user3@gmail.com' },
+        { name: 'User3', email: 'user3@gmail.com' },
+       
     ]
     res.json(users);
 })
